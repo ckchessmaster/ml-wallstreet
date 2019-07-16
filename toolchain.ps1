84768f2166ecfc3a378d8fb2fa3e48e7ef427c64
Chris Kingdon <ckchessmaster@gmail.com>
2019-07-16 19:07:31 -0400

    wip on toolchain

--- toolchain.ps1
+++ toolchain.ps1
@@ -1,1 +1,14 @@
-echo test!!!
+$folder = Resolve-Path "ML-Wallstreet-UI"
+Push-Location $folder
+dotnet publish -c Release
+Pop-Location
+
+
+$folder = Resolve-Path "DataManager-API"
+dotnet publish -c Release
+Pop-Location
+
+$folder = Resolve-Path "ML=Service-API"
+Push-Location $folder
+dotnet publish -c Release
+Pop-Location
