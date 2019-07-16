7e6aecb38092b9693905eb2962e12e58425b6e07
Brendan Reidy <brendanreidy@Brendans-MacBook-Pro-3.local>
2019-07-15 22:40:31 -0400

    added classes to assist with fetching data

---
+++ MLService-API/MLService-API/Services/IModelService.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace MLServiceAPI.Services
+{
+    public interface IModelService
+    {
+    }
+}
---
+++ MLService-API/MLService-API/Services/ValueService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MLServiceAPI.Services
+{
+    public class ValueService
+    {
+        public ValueService()
+        {
+        }
+    }
+}
