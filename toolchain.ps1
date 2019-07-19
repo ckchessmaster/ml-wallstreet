b0173afe99ab9494f228ab146899bbc2ae232347
Chris Kingdon <ckchessmaster@gmail.com>
2019-07-18 23:38:48 -0400

    test

--- toolchain.ps1
+++ toolchain.ps1
@@ -1,8 +1,1 @@
-Stop-WebAppPool -Name "ml-wallstreet-ui"
-$folder = Resolve-Path "ML-Wallstreet-UI"
-Push-Location $folder
-dotnet publish -c Release --self-contained -r win-x64
-Move-Item -Path .\ML-Wallstreet-UI\bin\Release\netcoreapp2.2\win-x64\publish -Destination ..\..\hosting\ML-Wallstreet-UI
-Pop-Location
-Start-WebAppPool -Name "ml-wallstreet-ui"
-
+echo Get-Location
