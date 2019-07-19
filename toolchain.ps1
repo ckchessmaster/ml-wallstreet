418a97652646119bc1f116c5795f339ddd34c200
Chris Kingdon <ckchessmaster@gmail.com>
2019-07-18 23:56:39 -0400

    test

--- toolchain.ps1
+++ toolchain.ps1
@@ -4,7 +4,7 @@ Stop-WebAppPool -Name "ml-wallstreet-ui"
 $folder = $basePath + "ML-Wallstreet-UI"
 Push-Location $folder
 dotnet publish -c Release --self-contained -r win-x64
-Remove-Item -Path ..\..\hosting\ML-Wallstreet-UI
+Remove-Item -Force -Path ..\..\hosting\ML-Wallstreet-UI
 Move-Item -Force -Path .\ML-Wallstreet-UI\bin\Release\netcoreapp2.2\win-x64\publish -Destination ..\..\hosting\ML-Wallstreet-UI
 Pop-Location
 Start-WebAppPool -Name "ml-wallstreet-ui"
