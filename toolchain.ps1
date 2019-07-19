c97581046414e8beaaec8905078cec8556932f7e
Chris Kingdon <ckchessmaster@gmail.com>
2019-07-18 23:45:27 -0400

    test

--- toolchain.ps1
+++ toolchain.ps1
@@ -1,1 +1,9 @@
-Get-Location
+$basePath = "C:\Users\ckche\Desktop\ML Stock Project\ml-wallstreet\"
+
+Stop-WebAppPool -Name "ml-wallstreet-ui"
+$folder = $basePath + "ML-Wallstreet-UI"
+Push-Location $folder
+dotnet publish -c Release --self-contained -r win-x64
+Move-Item -Path .\ML-Wallstreet-UI\bin\Release\netcoreapp2.2\win-x64\publish -Destination ..\..\hosting\ML-Wallstreet-UI
+Pop-Location
+Start-WebAppPool -Name "ml-wallstreet-ui"
