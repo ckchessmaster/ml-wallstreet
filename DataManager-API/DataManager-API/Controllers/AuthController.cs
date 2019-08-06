30f223ceb7b36de493b1d9e9d2b6e379e634cff5
Chris Kingdon <ckchessmaster@gmail.com>
2019-08-05 23:16:58 -0400

    wip

---
+++ DataManager-API/DataManager-API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MLWCore.Security;
+using Microsoft.Extensions.Configuration;
+
+namespace DataManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IConfiguration config;
+
+        public AuthController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet]
+        [Route("getToken")]
+        public IActionResult GetToken([FromHeader(Name = "Api-Key")]string apiKey)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                return new BadRequestResult();
+            }
+
+
+
+            return new JsonResult(new { Token = apiKey });
+        }
+    }
+}
---
+++ MLWCore/MLWCore/MLWCore/Security/Password.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLWCore.Security
+{
+    public class Password
+    {
+        public string Hash { get; set; }
+
+        public string Salt { get; set; }
+    }
+}
