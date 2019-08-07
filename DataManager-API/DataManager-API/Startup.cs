ab70006e7cda4e1621330159cca5b0056d4eebd9
Chris Kingdon <ckchessmaster@gmail.com>
2019-08-07 00:18:32 -0400

    added authorization to the mlservice api

--- DataManager-API/DataManager-API/Startup.cs
+++ DataManager-API/DataManager-API/Startup.cs
@@ -60,7 +60,6 @@ namespace DataManagerAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvc();
         }
---
+++ MLService-API/MLService-API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using MLWCore.Security;
+
+namespace MLServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IConfiguration config;
+
+        public AuthController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet]
+        [Route("getToken")]
+        public IActionResult GetToken([FromHeader(Name = "Api-Key")]string apiKey)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                return new BadRequestResult();
+            }
+
+            // Hash the apiKey
+            var dataManagerApiKey = new Password
+            {
+                Hash = this.config.GetValue<string>("Security:ApiKey:Hash"),
+                Salt = this.config.GetValue<string>("Security:ApiKey:Salt")
+            };
+
+            Password hashedKey = SecurityHelper.HashPassword(apiKey, dataManagerApiKey.Salt);
+
+            if (hashedKey.Equals(dataManagerApiKey))
+            {
+                return new JsonResult(new { Token = SecurityHelper.GenerateToken(this.config.GetValue<string>("Security:SigningKey")) });
+            }
+
+            return new UnauthorizedResult();
+        }
+    }
+}
