90167f6568207c9d14d1698699f337afc9c0a507
Chris Kingdon <ckchessmaster@gmail.com>
2019-09-04 21:58:54 -0400

    completed UI for data retrieval

---
+++ ML-Wallstreet-UI/MLWallstreetUI/Services/ApiService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using MLWallstreetUI.Data.Enums;
+using MLWallstreetUI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MLWallstreetUI.Services
+{
+    public class ApiService
+    {
+        private readonly Dictionary<ServiceApiType, ServiceApi> serviceApis;
+
+        public ApiService(IConfiguration configuration)
+        {
+            serviceApis = new Dictionary<ServiceApiType, ServiceApi>
+            {
+                { ServiceApiType.DataManagerApi, new ServiceApi(configuration, configuration.GetValue<string>("DataManagerApi:Url")) },
+                { ServiceApiType.MLServiceApi, new ServiceApi(configuration, "") }
+            };
+        }
+
+        public async Task<ServiceApi> GetApi(ServiceApiType api)
+        {
+            if (!serviceApis[api].IsAuthenticated())
+            {
+                await serviceApis[api].Authenticate();
+            }
+
+            return serviceApis[api];
+        }
+    }
+}
