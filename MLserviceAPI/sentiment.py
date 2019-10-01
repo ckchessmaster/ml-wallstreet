ecd055c330b0502f8702751e2be493f5fe1ad422
Chris Kingdon <ckchessmaster@gmail.com>
2019-09-30 22:29:22 -0400

    Got single sentiment prediction working

--- MLserviceAPI/sentiment.py
+++ MLserviceAPI/sentiment.py
@@ -2,7 +2,14 @@ from flask import Blueprint
 from flask import jsonify
 from flask import request
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
 import pickle
+import re
 
 sentiment_api = Blueprint('sentiment_api', __name__)
 
@@ -13,5 +20,25 @@ def predict_single():
     if inputText is None:
         return jsonify({"Message":"Missing required query parameter: InputText"}), 400
 
+    # Clean the input text
+    cleanText = re.sub('[^a-zA-Z]', ' ', inputText) # Replace all non letters with spaces
+    cleanText = cleanText.lower() # Set the entire text to lower case
+    cleanText = cleanText.split() # Split the text into it's individual words
+
+    # Remove words that don't contribute anything (like the, a, this, etc.)
+    # Also apply stemming aka getting the root of words
+    ps = PorterStemmer()
+    cleanText = [ps.stem(word) for word in cleanText if not word in set(stopwords.words('english'))]
+    cleanText = ' '.join(cleanText) # Put the string back together
+
+    # Predict
+    vectorizer = pickle.load(open('sentiment.vec', 'rb'))
+    textFinal = vectorizer.transform([cleanText]).toarray()
     classifier = pickle.load(open('sentiment.mdl', 'rb'))
-    return jsonify({ "Result": classifier.predict(inputText) })
+    result = classifier.predict(textFinal).tolist()
+    finalResult = False
+
+    if result[0] == 1:
+        finalResult = True
+
+    return jsonify({ "Result": finalResult })
