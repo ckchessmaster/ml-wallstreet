673c1ab381f207405af3b22a29ed1c297d9720bc
Chris Kingdon <ckchessmaster@gmail.com>
2019-10-31 17:13:28 -0400

    finally got middleware working!!!!!

--- MLserviceAPI/src/app.py
+++ MLserviceAPI/src/app.py
@@ -4,19 +4,22 @@
 import os
 os.chdir(os.path.dirname(__file__) + '/..')
 
+# Imports
 from flask import Flask
 from routes.health import health_api
 from routes.sentiment import sentiment_api
 
 import middleware.authmiddleware as middleware
 
+# Configure the API
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 # 100MB
 
-app.wsgi_app = middleware.AuthMiddleware(app.wsgi_app)
+app.wsgi_app = middleware.AuthMiddleware(app)
 
 app.register_blueprint(health_api, url_prefix='/api/health')
 app.register_blueprint(sentiment_api, url_prefix='/api/sentiment')
 
+# Run the app
 if __name__ == '__main__':
     app.run()
--- MLserviceAPI/src/middleware/authmiddleware.py
+++ MLserviceAPI/src/middleware/authmiddleware.py
@@ -4,6 +4,7 @@ import json
 import http.client
 import ssl
 
+# TODO: We need to make the bad requests actually return a body. WSGI is a pain...
 class AuthMiddleware(object):
     def __init__(self, app):
         self.app = app
@@ -13,30 +14,52 @@ class AuthMiddleware(object):
         if environ['PATH_INFO'].startswith('/api/health'):
             return self.app(environ, start_response)
         
-        responseHeaders = [('Content-type', 'application/json')]
-        body = '{}'
+        # TODO: mabye make this configurable? Or just rely on the requested content type.
+        content_type = 'application/json'
         
         # If there was no token at all then this is a bad request
         if not 'HTTP_AUTHORIZATION' in environ:
-            status = '400 Bad Request'
-            start_response(status, responseHeaders)
-            return [body]
+            body = json.dumps({"message":"Bad Request. No token provided."})
+            
+            response = self.app.make_response(body)
+            response.content_type = content_type
+            response.status_code = 400
+            
+            return response(environ, start_response)
             
         # Validate the token with the security service
         url = config.MLWSECURITYSERVICE_URL
+
         # TODO: context should not allow self signed certs, this is only for dev
-        con = http.client.HTTPSConnection(url, timeout=10, context = ssl._create_unverified_context())
-        requestHeaders = {'Content-type': 'application/json'}
-        requestPayload = environ['HTTP_AUTHORIZATION']
-        json_data = json.dumps(requestPayload)
-        con.request('POST', '/api/auth/validateToken', json_data, requestHeaders)
-        response = con.getresponse()
-        result = json.loads(response.read().decode())
-        con.close()
+        try:
+            con = http.client.HTTPSConnection(url, timeout=10, context = ssl._create_unverified_context())
+            requestHeaders = {'Content-type': 'application/json'}
+            requestPayload = environ['HTTP_AUTHORIZATION']
+            json_data = json.dumps({"token":requestPayload})
+            con.request('POST', '/api/auth/validateToken', json_data, requestHeaders)
+            response = con.getresponse()
+            result = json.loads(response.read().decode())
+            con.close()
+        except Exception as e:
+            # TODO: Add proper logging
+            print(e)
+
+            body = json.dumps({"message":"Something went wrong. Please try again later."})
+            
+            response = self.app.make_response(body)
+            response.content_type = content_type
+            response.status_code = 500
+
+            return response(environ, start_response)
         
         if (response.status != 200 or result['result'] == False):
-            status = '401 Unauthorized'
-            start_response(status, responseHeaders)
-            return [body]
+            body = json.dumps({"message":"Access denied. Invalid token."})
+            
+            response = self.app.make_response(body)
+            response.content_type = content_type
+            response.status_code = 401
+
+            return response(environ, start_response)
         
+        # Token is valid
         return self.app(environ, start_response)
