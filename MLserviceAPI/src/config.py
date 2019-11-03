033b38b3d848f6e5bf98f71b9efea14cbe93904c
Chris Kingdon <ckchessmaster@gmail.com>
2019-11-03 09:32:41 -0500

    hiding config for future use and adding environment config

--- MLserviceAPI/src/config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# General config
-MLWSECURITYSERVICE_URL = 'localhost:44373'
-
-# Sentiment config
-BAG_OF_WORDS_SIZE = 1500
--- MLserviceAPI/src/middleware/authmiddleware.py
+++ MLserviceAPI/src/middleware/authmiddleware.py
@@ -20,6 +20,10 @@ class AuthMiddleware(object):
         return (f'Invalid login attempt! REQUEST_METHOD: {request_method}, REQUEST_URI: {request_uri}, REMOTE_ADDR: {remote_addr}')
 
     def __call__(self, environ, start_response):
+        # If we are in a dev environment don't worry about security
+        if config.ENVIRONMNET == 'DEV':
+            return self.app(environ, start_response)
+
         # The health route is the only route which does not need authentication
         if environ['PATH_INFO'].startswith('/api/health'):
             return self.flask(environ, start_response)
