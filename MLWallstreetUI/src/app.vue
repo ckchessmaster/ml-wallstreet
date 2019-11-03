0b30e3b4de59f872e3f2b1fa09c9745831d3ceb8
Chris Kingdon <ckchessmaster@gmail.com>
2019-11-03 16:05:42 -0500

    More initial setup and creating login.

--- MLWallstreetUI/src/app.vue
+++ MLWallstreetUI/src/app.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="mlwallstreet">
+    <v-content>
+      <Login />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login';
 
 export default {
-  name: 'app',
+  name: 'MLWallstreet',
   components: {
-    HelloWorld
+    Login,
+  },
+  data: () => ({}),
+  created() {
+    this.$vuetify.theme.dark = true
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- MLWallstreetUI/src/plugins/vuetify.js
+++ MLWallstreetUI/src/plugins/vuetify.js
@@ -7,4 +7,26 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
+  theme: {
+    themes: {
+      light: {
+        primary: '#8bc34a',
+        secondary: '#607d8b',
+        accent: '#795548',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#009688',
+        success: '#cddc39'
+      },
+      dark: {
+        primary: '#8bc34a',
+        secondary: '#607d8b',
+        accent: '#795548',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#009688',
+        success: '#cddc39'
+      }
+    }
+  }
 });
